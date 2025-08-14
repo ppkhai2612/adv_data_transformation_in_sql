cd17e2402af84717766955a0b7df0bc53e3f2a88
ppkhai2612 <117285386+ppkhai2612@users.noreply.github.com>
2025-08-14 03:16:22 +0000

    init sql advanced

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,8 @@
+-- installs an extension or module that provides TPC-H functionalities
 INSTALL tpch;
+-- activates or loads that extension into the current session
 LOAD tpch;
+-- generates synthetic data for the TPC-H benchmark (approximately 1GB in size)
 CALL dbgen(sf = 1);
+-- lists all the tables that currently exist in database
 SHOW TABLES;
-
